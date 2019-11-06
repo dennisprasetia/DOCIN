71b8207e01fcb2f4bbd93fca3fdcb55adebe1eb0
Dennis Prasetia <dennis.prasetia@gmail.com>
2019-11-06 09:21:15 +0700

    Add realisation fragment into schema and fix name

--- app/src/main/java/com/wonokoyo/docin/menu/RealisationFragment.java
+++ app/src/main/java/com/wonokoyo/docin/menu/RealisationFragment.java
@@ -11,10 +11,10 @@ import android.view.ViewGroup;
 
 import com.wonokoyo.docin.R;
 
-public class RealitationFragment extends Fragment {
+public class RealisationFragment extends Fragment {
 
 
-    public RealitationFragment() {
+    public RealisationFragment() {
         // Required empty public constructor
     }
 
@@ -23,7 +23,7 @@ public class RealitationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_realitation, container, false);
+        return inflater.inflate(R.layout.fragment_realisation, container, false);
     }
 
 }
