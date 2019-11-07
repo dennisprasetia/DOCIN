ff00ac68089ac58ad18f41256c19dfc75782612b
Dennis Prasetia <dennis.prasetia@gmail.com>
2019-11-07 15:28:38 +0700

    Add internet request permission to get access

--- app/src/main/java/com/wonokoyo/docin/menu/HomeFragment.java
+++ app/src/main/java/com/wonokoyo/docin/menu/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.wonokoyo.docin.menu;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,25 +15,39 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.wonokoyo.docin.R;
 
 public class HomeFragment extends Fragment {
+
+    private static final int REQUEST_INTERNET_CODE = 100;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        requestPermissions(new String[]{Manifest.permission.INTERNET}, REQUEST_INTERNET_CODE);
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_INTERNET_CODE) {
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                Snackbar.make(this.getView(), "Apps need internet to run", 3);
+            }
+        }
     }
 }
